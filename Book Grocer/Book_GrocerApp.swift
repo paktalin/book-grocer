fac2ce74bd00120b51540cba47b6f98f5e67687f
Lita Kornilova <lita.kornilova@fob-solutions.com>
2021-07-02 15:49:37 +0300

    Initial Commit

---
+++ Book Grocer/Book_GrocerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Book_GrocerApp.swift
+//  Book Grocer
+//
+//  Created by Lita Kornilova on 02.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct Book_GrocerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
